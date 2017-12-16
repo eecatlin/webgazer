fd053caebbef4e3fe9abb2a4e88aa5f0c075a4d0
Oliver Hare <ohare@cs.brown.edu>
2017-12-16 16:38:34 -0500

    final results of CNN, along with final parameters

--- WebGazer-master/src/gazerCNN.py
+++ WebGazer-master/src/gazerCNN.py
@@ -3,28 +3,29 @@ import csv
 import cv2
 import numpy as np
 import tensorflow as tf
+import random as rand
+import time
 from time import gmtime, strftime
 
 with open('CNNresults/results.csv', 'a') as csvfile:
 	wr = csv.writer(csvfile, dialect='excel')
-	wr.writerow(['Time Stamp:', strftime("%Y-%m-%d %H:%M:%S", gmtime())])
+	wr.writerow(['Session Time Stamp: ' + str(strftime("%Y-%m-%d %H:%M:%S", gmtime()))])
 
 ######################################################################################
 ##################################### Parameters #####################################
 ######################################################################################
 
-# https://arxiv.org/pdf/1605.05258.pdf
-# https://web.stanford.edu/class/cs231a/prev_projects_2016/eye-display-gaze-2.pdf
-
-batchSz = 8
+batchSz = 1
 imgW = 50
 imgH = 42
 epochNum = 1
-learnRate = 0.001
+learnRate = 0.0001
 
 ######################################################################################
 ################################## CNN Architecture ##################################
 ######################################################################################
+
+# https://arxiv.org/pdf/1605.05258.pdf
 
 imgBatch = tf.placeholder(tf.float32, [batchSz, imgH, imgW, 1])
 labels = tf.placeholder(tf.float32, [batchSz, 1])
@@ -136,73 +137,91 @@ for i in range(65):
 			# Gets the path of the gazePredictions.csv so to run through it and build data points
 			predPath = '/course/cs143/datasets/webgazer/framesdataset/' + path + '/gazePredictions.csv'
 
-			with open(predPath) as f:
-				readCSV = csv.reader(f, delimiter=',')
-				# Goes through each row of the CSV
-				for row in readCSV:
-					framesProcessed += 1
-					if framesProcessed % 5000 == 0:
-						print('Collected data from %d frames' % framesProcessed)
+			try:
+				with open(predPath) as f:
+					readCSV = csv.reader(f, delimiter=',')
+					# Goes through each row of the CSV
+					for row in readCSV:
+						framesProcessed += 1
+						#if framesProcessed % 5000 == 0:
+						#	print('Collected data from %d frames' % framesProcessed)
 
-					# Grab the file name, tobii results, webgzer results, and setup the clmTracker data
-					frameFilename = row[0]
-					frameTimestamp = row[1]
-					tobiiLeftEyeGazeX = float( row[2] )
-					tobiiLeftEyeGazeY = float( row[3] )
-					tobiiRightEyeGazeX = float( row[4] )
-					tobiiRightEyeGazeY = float( row[5] )
-					webgazerX = float( row[6] )
-					webgazerY = float( row[7] )
-					clmTracker = row[8:len(row)-1]
-					clmTracker = [float(m) for m in clmTracker]
-					clmTrackerInt = [int(m) for m in clmTracker]
+						# Grab the file name, tobii results, webgzer results, and setup the clmTracker data
+						frameFilename = row[0]
+						frameTimestamp = row[1]
+						tobiiLeftEyeGazeX = float( row[2] )
+						tobiiLeftEyeGazeY = float( row[3] )
+						tobiiRightEyeGazeX = float( row[4] )
+						tobiiRightEyeGazeY = float( row[5] )
+						webgazerX = float( row[6] )
+						webgazerY = float( row[7] )
+						clmTracker = row[8:len(row)-1]
+						clmTracker = [float(m) for m in clmTracker]
+						clmTrackerInt = [int(m) for m in clmTracker]
 
-					tobiiEyeGazeX = (tobiiLeftEyeGazeX + tobiiRightEyeGazeX) / 2
-					tobiiEyeGazeY = (tobiiLeftEyeGazeY + tobiiRightEyeGazeY) / 2
+						tobiiEyeGazeX = (tobiiLeftEyeGazeX + tobiiRightEyeGazeX) / 2
+						tobiiEyeGazeY = (tobiiLeftEyeGazeY + tobiiRightEyeGazeY) / 2
 
-					# Gets the eye mid points and the top left corner of each eye region
-					lEyeMidY = clmTrackerInt[54]
-					lEyeMidX = clmTrackerInt[55]
-					rEyeMidY = clmTrackerInt[64]
-					rEyeMidX = clmTrackerInt[65]
-					lEyeCornerY = max(lEyeMidY - (imgH // 2), 0)
-					lEyeCornerX = max(lEyeMidX - (imgW // 2), 0)
-					rEyeCornerY = max(rEyeMidY - (imgH // 2), 0)
-					rEyeCornerX = max(rEyeMidX - (imgW // 2), 0)
+						# Gets the eye mid points and the top left corner of each eye region
+						lEyeMidY = clmTrackerInt[54]
+						lEyeMidX = clmTrackerInt[55]
+						rEyeMidY = clmTrackerInt[64]
+						rEyeMidX = clmTrackerInt[65]
+						lEyeCornerY = max(lEyeMidY - (imgH // 2), 0)
+						lEyeCornerX = max(lEyeMidX - (imgW // 2), 0)
+						rEyeCornerY = max(rEyeMidY - (imgH // 2), 0)
+						rEyeCornerX = max(rEyeMidX - (imgW // 2), 0)
 
-					# reads in the image for the specific frame
-					image = cv2.imread('/course/cs143/datasets/webgazer/framesdataset/' + frameFilename[2:],0) / 255
+						# reads in the image for the specific frame
+						image = cv2.imread('/course/cs143/datasets/webgazer/framesdataset/' + frameFilename[2:],0) / 255
+						yub, xub = np.shape(image)
 
-					# grab the two eye images
-					lEye = image[lEyeCornerY:lEyeCornerY + imgH, lEyeCornerX:lEyeCornerX + imgW]
-					rEye = image[rEyeCornerY:rEyeCornerY + imgH, rEyeCornerX:rEyeCornerX + imgW]
-					# Concatenates the two images into a single one
-					# (I don't use this, because I don't train on one image of both eyes
-					# But it's useful to build anyways to debug whether the eye images are ok)
-					try:
-						bEye = np.concatenate((lEye, rEye), axis=1)
-					except:
-						print('Eye Concat Error')
-						print(np.shape(lEye))
-						print(np.shape(rEye))
-						cv2.imshow('image', lEye)
-						cv2.waitKey(0)
-						cv2.imshow('image', rEye)
-						cv2.waitKey(0)
+						# Bounds	
+						if lEyeCornerY + imgH >= yub:
+							lEyeCornerY = yub - imgH
+						if lEyeCornerX + imgW >= xub:
+							lEyeCornerX = xub - imgW
+						if rEyeCornerY + imgH >= yub:
+							rEyeCornerY = yub - imgH
+						if rEyeCornerX + imgW >= xub:
+							rEyeCornerX = xub - imgW
 
-					# append each data point to its relevant data set
-					# currently neither training nor testing uses trainEyes, and I never use
-					# the trainLabelsY or trainLabelsX for training, though their corresponding
-					# values get used in testing
-					trainREyes.append(rEye)
-					trainLEyes.append(lEye)
-					trainEyes.append(bEye)
-					trainRLabelsY.append(tobiiRightEyeGazeY)
-					trainRLabelsX.append(tobiiRightEyeGazeX)
-					trainLLabelsY.append(tobiiLeftEyeGazeY)
-					trainLLabelsX.append(tobiiLeftEyeGazeX)
-					trainLabelsY.append(tobiiEyeGazeY)
-					trainLabelsX.append(tobiiEyeGazeX)
+						# grab the two eye images
+						lEye = image[lEyeCornerY:lEyeCornerY + imgH, lEyeCornerX:lEyeCornerX + imgW]
+						rEye = image[rEyeCornerY:rEyeCornerY + imgH, rEyeCornerX:rEyeCornerX + imgW]
+						# Concatenates the two images into a single one
+						# (I don't use this, because I don't train on one image of both eyes
+						# But it's useful to build anyways to debug whether the eye images are ok)
+						try:
+							bEye = np.concatenate((lEye, rEye), axis=1)
+						except:
+							print('Eye Concat Error')
+							print(lEyeCornerY)
+							print(lEyeCornerX)
+							print(rEyeCornerY)
+							print(rEyeCornerX)
+							print(np.shape(lEye))
+							print(np.shape(rEye))
+							cv2.imshow('image', lEye)
+							cv2.waitKey(0)
+							cv2.imshow('image', rEye)
+							cv2.waitKey(0)
+
+						# append each data point to its relevant data set
+						# currently neither training nor testing uses trainEyes, and I never use
+						# the trainLabelsY or trainLabelsX for training, though their corresponding
+						# values get used in testing
+						trainREyes.append(rEye)
+						trainLEyes.append(lEye)
+						trainEyes.append(bEye)
+						trainRLabelsY.append(tobiiRightEyeGazeY)
+						trainRLabelsX.append(tobiiRightEyeGazeX)
+						trainLLabelsY.append(tobiiLeftEyeGazeY)
+						trainLLabelsX.append(tobiiLeftEyeGazeX)
+						trainLabelsY.append(tobiiEyeGazeY)
+						trainLabelsX.append(tobiiEyeGazeX)
+			except:
+				print('No webGazerPredictions.csv')
 
 		print('Building Testing Data')
 
@@ -210,61 +229,79 @@ for i in range(65):
 		for path in pTestPaths:
 			predPath = '/course/cs143/datasets/webgazer/framesdataset/' + path + '/gazePredictions.csv'
 
-			with open(predPath) as f:
-				readCSV = csv.reader(f, delimiter=',')
-				for row in readCSV:
+			try:
+				with open(predPath) as f:
+					readCSV = csv.reader(f, delimiter=',')
+					for row in readCSV:
 
-					frameFilename = row[0]
-					frameTimestamp = row[1]
-					tobiiLeftEyeGazeX = float( row[2] )
-					tobiiLeftEyeGazeY = float( row[3] )
-					tobiiRightEyeGazeX = float( row[4] )
-					tobiiRightEyeGazeY = float( row[5] )
-					webgazerX = float( row[6] )
-					webgazerY = float( row[7] )
-					clmTracker = row[8:len(row)-1]
-					clmTracker = [float(m) for m in clmTracker]
-					clmTrackerInt = [int(m) for m in clmTracker]
+						frameFilename = row[0]
+						frameTimestamp = row[1]
+						tobiiLeftEyeGazeX = float( row[2] )
+						tobiiLeftEyeGazeY = float( row[3] )
+						tobiiRightEyeGazeX = float( row[4] )
+						tobiiRightEyeGazeY = float( row[5] )
+						webgazerX = float( row[6] )
+						webgazerY = float( row[7] )
+						clmTracker = row[8:len(row)-1]
+						clmTracker = [float(m) for m in clmTracker]
+						clmTrackerInt = [int(m) for m in clmTracker]
 
-					tobiiEyeGazeX = (tobiiLeftEyeGazeX + tobiiRightEyeGazeX) / 2
-					tobiiEyeGazeY = (tobiiLeftEyeGazeY + tobiiRightEyeGazeY) / 2
+						tobiiEyeGazeX = (tobiiLeftEyeGazeX + tobiiRightEyeGazeX) / 2
+						tobiiEyeGazeY = (tobiiLeftEyeGazeY + tobiiRightEyeGazeY) / 2
 
-					lEyeMidY = clmTrackerInt[54]
-					lEyeMidX = clmTrackerInt[55]
-					rEyeMidY = clmTrackerInt[64]
-					rEyeMidX = clmTrackerInt[65]
-					lEyeCornerY = max(lEyeMidY - (imgH // 2), 0)
-					lEyeCornerX = max(lEyeMidX - (imgW // 2), 0)
-					rEyeCornerY = max(rEyeMidY - (imgH // 2), 0)
-					rEyeCornerX = max(rEyeMidX - (imgW // 2), 0)
+						lEyeMidY = clmTrackerInt[54]
+						lEyeMidX = clmTrackerInt[55]
+						rEyeMidY = clmTrackerInt[64]
+						rEyeMidX = clmTrackerInt[65]
+						lEyeCornerY = max(lEyeMidY - (imgH // 2), 0)
+						lEyeCornerX = max(lEyeMidX - (imgW // 2), 0)
+						rEyeCornerY = max(rEyeMidY - (imgH // 2), 0)
+						rEyeCornerX = max(rEyeMidX - (imgW // 2), 0)
 
-					image = cv2.imread('/course/cs143/datasets/webgazer/framesdataset/' + frameFilename[2:],0) / 255
+						image = cv2.imread('/course/cs143/datasets/webgazer/framesdataset/' + frameFilename[2:],0) / 255
+						yub, xub = np.shape(image)
 
-					lEye = image[lEyeCornerY:lEyeCornerY + imgH, lEyeCornerX:lEyeCornerX + imgW]
-					rEye = image[rEyeCornerY:rEyeCornerY + imgH, rEyeCornerX:rEyeCornerX + imgW]
-					try:
-						bEye = np.concatenate((lEye, rEye), axis=1)
-					except:
-						print('Eye Concat Error')
-						print(np.shape(lEye))
-						print(np.shape(rEye))
-						cv2.imshow('image', lEye)
-						cv2.waitKey(0)
-						cv2.imshow('image', rEye)
-						cv2.waitKey(0)
+						if lEyeCornerY + imgH >= yub:
+							lEyeCornerY = yub - imgH
+						if lEyeCornerX + imgW >= xub:
+							lEyeCornerX = xub - imgW
+						if rEyeCornerY + imgH >= yub:
+							rEyeCornerY = yub - imgH
+						if rEyeCornerX + imgW >= xub:
+							rEyeCornerX = xub - imgW
 
-					testREyes.append(rEye)
-					testLEyes.append(lEye)
-					testEyes.append(bEye)
-					testRLabelsY.append(tobiiRightEyeGazeY)
-					testRLabelsX.append(tobiiRightEyeGazeX)
-					testLLabelsY.append(tobiiLeftEyeGazeY)
-					testLLabelsX.append(tobiiLeftEyeGazeX)
-					testLabelsY.append(tobiiEyeGazeY)
-					testLabelsX.append(tobiiEyeGazeX)
+						lEye = image[lEyeCornerY:lEyeCornerY + imgH, lEyeCornerX:lEyeCornerX + imgW]
+						rEye = image[rEyeCornerY:rEyeCornerY + imgH, rEyeCornerX:rEyeCornerX + imgW]
+						try:
+							bEye = np.concatenate((lEye, rEye), axis=1)
+						except:
+							print('Eye Concat Error')
+							print(lEyeCornerY)
+							print(lEyeCornerX)
+							print(rEyeCornerY)
+							print(rEyeCornerX)
+							print(np.shape(lEye))
+							print(np.shape(rEye))
+							
+							cv2.imshow('image', lEye)
+							cv2.waitKey(0)
+							cv2.imshow('image', rEye)
+							cv2.waitKey(0)
 
-					testGazerY.append(webgazerY)
-					testGazerX.append(webgazerX)
+						testREyes.append(rEye)
+						testLEyes.append(lEye)
+						testEyes.append(bEye)
+						testRLabelsY.append(tobiiRightEyeGazeY)
+						testRLabelsX.append(tobiiRightEyeGazeX)
+						testLLabelsY.append(tobiiLeftEyeGazeY)
+						testLLabelsX.append(tobiiLeftEyeGazeX)
+						testLabelsY.append(tobiiEyeGazeY)
+						testLabelsX.append(tobiiEyeGazeX)
+
+						testGazerY.append(webgazerY)
+						testGazerX.append(webgazerX)
+			except:
+				print('No webGazerPredictions.csv')
 
 		trainNum = len(trainLabelsY)
 		testNum = len(testLabelsY)
@@ -272,6 +309,8 @@ for i in range(65):
 		####################################### Training #######################################
 
 		print('Training')
+
+		trainingStartTime = time.time()
 
 		# Left Eye Y
 		print('Training Left Eye Y')
@@ -286,8 +325,8 @@ for i in range(65):
 
 			for j in range(trainNum // batchSz):
 				framesProcessed += 1
-				if framesProcessed % 1000 == 0:
-					print('Frames Batches Processed:', framesProcessed)
+				#if framesProcessed % 1000 == 0:
+				#	print('Frames Batches Processed:', framesProcessed)
 
 				# Build the imgBatch x, and labels y, then feed them into the feedDict and train
 				x = []
@@ -296,9 +335,9 @@ for i in range(65):
 				x = np.array(x).reshape(batchSz, imgH, imgW, 1)
 				y = np.array(trainLLabelsY[j * batchSz:(j + 1) * batchSz]).reshape(batchSz, 1)
 				debugloss, logs, _ = sess.run([loss, logits, train], feed_dict={imgBatch: x, labels: y})
-				if framesProcessed % 100 == 0:
-					print('debug loss:', debugloss)
-					print(logs)
+				# if framesProcessed % 100 == 0:
+				#	print('debug loss:', debugloss)
+				#	print(logs)
 
 		# Save the relevant model for the eye and coordinate
 		saver.save(sess, "CNNmodels/leftEyeY.ckpt")
@@ -314,8 +353,8 @@ for i in range(65):
 
 			for j in range(trainNum // batchSz):
 				framesProcessed += 1
-				if framesProcessed % 1000 == 0:
-					print('Frames Batches Processed:', framesProcessed)
+				#if framesProcessed % 1000 == 0:
+				#	print('Frames Batches Processed:', framesProcessed)
 
 				x = []
 				for k in range(j * batchSz, (j + 1) * batchSz):
@@ -337,8 +376,8 @@ for i in range(65):
 
 			for j in range(trainNum // batchSz):
 				framesProcessed += 1
-				if framesProcessed % 1000 == 0:
-					print('Frames Batches Processed:', framesProcessed)
+				#if framesProcessed % 1000 == 0:
+				#	print('Frames Batches Processed:', framesProcessed)
 
 				x = []
 				for k in range(j * batchSz, (j + 1) * batchSz):
@@ -360,8 +399,8 @@ for i in range(65):
 
 			for j in range(trainNum // batchSz):
 				framesProcessed += 1
-				if framesProcessed % 1000 == 0:
-					print('Frames Batches Processed:', framesProcessed)
+				#if framesProcessed % 1000 == 0:
+				#	print('Frames Batches Processed:', framesProcessed)
 
 				x = []
 				for k in range(j * batchSz, (j + 1) * batchSz):
@@ -371,6 +410,8 @@ for i in range(65):
 				sess.run(train, feed_dict={imgBatch: x, labels: y})
 
 		saver.save(sess, "CNNmodels/rightEyeX.ckpt")
+
+		trainingTime = time.time() - trainingStartTime
 
 		####################################### Testing #######################################
 
@@ -388,8 +429,8 @@ for i in range(65):
 		# Go through the test data
 		for j in range(testNum // batchSz):
 			testsProcessed += 1
-			if testsProcessed % 1000 == 0:
-				print('Frames Batches Processed:', testsProcessed)
+			#if testsProcessed % 1000 == 0:
+			#	print('Frames Batches Processed:', testsProcessed)
 
 			# Build x and y for the feedDict
 			x = []
@@ -499,6 +540,9 @@ for i in range(65):
 		print('Average Right X Distance:', avgTestRightXDist)
 		print('Average Left Y Distance:', avgTestLeftYDist)
 		print('Average Right Y Distance:', avgTestRightYDist)
+		print('Training Time:', trainingTime)
+		print('Size of Training Set:', trainNum)
+		print('Size of Testing Set:', testNum)
 		print('~')
 
 		with open('CNNresults/results.csv', 'a') as csvfile:
@@ -514,5 +558,8 @@ for i in range(65):
 				avgTestRightXDist,
 				avgGazerDist, 
 				avgGazerYDist, 
-				avgGazerXDist])
+				avgGazerXDist,
+				trainingTime,
+				trainNum,
+				testNum])
 
